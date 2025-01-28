62ee96d4d54e7bf15b17bedac00b466ac0e62df0
Cesar Choquehuanca <cesarchoqueskater@gmail.com>
2025-01-28 16:54:25 -0500

    1er commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TuModeloDeClientes+Choquehuanca',
+    version='0.0.1',
+    author='Cesar',
+    author_email='cesarchoqueskater@gmail.com',
+    description='TuModeloDeClientes',
+    packages=find_packages()
+)
+
